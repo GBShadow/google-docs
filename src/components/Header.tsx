0e9c99d8ddc42d3d1c02940cba435722bcc1ec8a
GBShadow <gbs.shadow@gmail.com>
2021-08-09 20:44:46 -0300

    Fix style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,39 +21,43 @@ export default function Header() {
         <Icon name="menu" size="3xl" />
       </Button>
       <Icon name="description" size="5xl" color="blue" />
-      <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
+      <div className="flex justify-between items-center w-full ">
+        <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
 
-      <div
-        className="mx-5 md:mx-20 flex flex-grow items-center px-5
+        <div
+          className="mx-5 md:mx-20 hidden sm:flex flex-grow items-center px-5
       py-2 bg-gray-100 text-gray-600 rounded-lg
       focus-within:text-gray-600 focus-within:shadow-md"
-      >
-        <Icon name="search" size="3xl" color="darkgray" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="hidden md:flex md:flex-grow px-5 text-base bg-transparent outline-none"
-        />
+        >
+          <Icon name="search" size="3xl" color="darkgray" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="flex md:flex-grow px-5 text-base bg-transparent outline-none"
+          />
+        </div>
+
+        <div className="flex items-center">
+          <Button
+            color="gray"
+            buttonType="outline"
+            rounded={true}
+            iconOnly={true}
+            ripple="dark"
+            className="ml-5 md:ml-20 h-20 w-20 border-0"
+          >
+            <Icon name="apps" size="3xl" color="gray" />
+          </Button>
+
+          <img
+            loading="lazy"
+            onClick={() => signOut()}
+            className="cursor-pointer h-12 w-12 rounded-full ml-2"
+            src={session?.user?.image}
+            alt="My picture of profile"
+          />
+        </div>
       </div>
-
-      <Button
-        color="gray"
-        buttonType="outline"
-        rounded={true}
-        iconOnly={true}
-        ripple="dark"
-        className="ml-5 md:ml-20 h-20 w-20 border-0"
-      >
-        <Icon name="apps" size="3xl" color="gray" />
-      </Button>
-
-      <img
-        loading="lazy"
-        onClick={() => signOut()}
-        className="cursor-pointer h-12 w-12 rounded-full ml-2"
-        src={session?.user?.image}
-        alt="My picture of profile"
-      />
     </header>
   )
 }
